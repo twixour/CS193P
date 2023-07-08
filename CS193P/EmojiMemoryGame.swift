3dc5b6675d1ab0eb7d1a5cd8666dcbeda94a48cb
twixour <twixour@gmail.com>
2023-07-08 14:52:01 +0530

    "Removing the types in the closure for createCardContent in EmojiMemoryGame Class"

--- CS193P/EmojiMemoryGame.swift
+++ CS193P/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ import SwiftUI
 
 class EmojiMemoryGame {
     private var model: MemoryGame<String> =
-    MemoryGame<String>(numberOfPairsOfCards: 4, createCardContent: {(index: Int) -> String in  return "😀" })
+    MemoryGame<String>(numberOfPairsOfCards: 4, createCardContent: {index  in  return "😀" })
     
     init(model: MemoryGame<String>) {
         self.model = model
