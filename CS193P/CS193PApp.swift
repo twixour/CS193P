6f1f3f05eaa77ed60891e39b477f48fdb0e1a964
twixour <twixour@gmail.com>
2023-07-08 10:11:59 +0530

    Initial Commit

---
+++ CS193P/CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS193PApp.swift
+//  CS193P
+//
+//  Created by Raushan Kashyap on 08/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
