5b7ead3f84f4c13fbef6ec1f04050fea31c37af9
twixour <twixour@gmail.com>
2023-07-08 12:56:45 +0530

    "Added LazyVGrid instead of HStack for -> ForEach One issue you will notice is that when you click on all three items of one row that is all are facing down(cards) then the cards collapse"

--- CS193P/ContentView.swift
+++ CS193P/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State var emojiCount: Int = 4
     var body: some View {
         VStack {
-            HStack {
+            LazyVGrid(columns: [GridItem(), GridItem(),GridItem()]) {
                 ForEach(emojis[0 ..< emojiCount], id:\.self) { emoji in
                     CardView(content: emoji)
                 }
